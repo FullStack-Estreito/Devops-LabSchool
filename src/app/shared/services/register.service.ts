a20f85e359253d878ce7f88fef0b637f008d643f
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-07-18 13:55:21 -0300

    feature [registerStudent]: criação da página de registro de alunmos

--- src/app/shared/services/register.service.ts
+++ src/app/shared/services/register.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
+import { Student } from '../models/student.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  url = "http://localhost:3000/usuarios"
+  url = "http://localhost:3000"
 
   constructor(private httpClient: HttpClient) { }
 
   postUser(data: User){
-    return this.httpClient.post(this.url, data)
+    return this.httpClient.post(`${this.url}/usuarios`, data)
+  }
+
+  postStudent(data: Student){
+    return this.httpClient.post(`${this.url}/alunos`, data)
   }
 }
